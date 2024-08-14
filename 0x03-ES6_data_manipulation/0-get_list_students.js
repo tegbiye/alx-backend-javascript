9d53a3919dd58af7580810a688dbf5f92a6a7f6d
tegbiye <se.tegbabu.solomon@gmail.com>
2024-08-14 09:20:02 +0000

    Updated the eslint format

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,20 @@
 export default function getListStudents() {
-    const o1 = {
-      id: 1,
-      firstName: 'Guillaume',
-      location: 'San Francisco',
-   };
+  const o1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
 
-    const o2 = {
-      id: 2,
-      firstName: 'James',
-      location: 'Columbia',
-    };
-    const o3 = {
-      id: 5,
-      firstName: 'Serena',
-      location: 'San Francisco',
-    };
+  const o2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+  const o3 = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
 
-     return [o1, o2, o3];
+  return [o1, o2, o3];
 }
